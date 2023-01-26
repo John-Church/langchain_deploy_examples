bde1f23a671bec90a038d986da7045536fb894f3
Jonathon Belotti <jonathon@modal.com>
2023-01-26 12:04:24 -0500

    nits on modal api usage

--- Modal/example.py
+++ Modal/example.py
@@ -2,12 +2,12 @@ import modal
 import os
 import chain
 
-# create an image and imo=port needed dependencies
-image = modal.Image.debian_slim().pip_install("langchain").pip_install("openai").pip_install("python-dotenv")
-stub = modal.Stub()
+# create an image and install needed dependencies
+image = modal.Image.debian_slim().pip_install("langchain", "openai", "python-dotenv")
+stub = modal.Stub(image=image)
 
 # create a webhook endpoint that calls the chain; import the chain module itself. Import secrets defined in the modal app.
-@stub.webhook(image=image, mounts=modal.create_package_mounts(["chain"]), secret=modal.Secret.from_name("my-langchain-secrets"))
+@stub.webhook(mounts=modal.create_package_mounts(["chain"]), secret=modal.Secret.from_name("my-langchain-secrets"))
 def evaluate_lead_endpoint(name: str, key: str):
     if key == os.environ.get("ENDPOINT_AUTH_KEY"):
         result = chain.generate_title(name)
